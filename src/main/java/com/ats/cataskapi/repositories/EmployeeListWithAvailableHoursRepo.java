b6c429dc596ccfef6c00a11b7616f8baf5d187a6
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-08-21 14:16:53 +0530

    /Akshay

--- src/main/java/com/ats/cataskapi/repositories/EmployeeListWithAvailableHoursRepo.java
+++ src/main/java/com/ats/cataskapi/repositories/EmployeeListWithAvailableHoursRepo.java
@@ -11,7 +11,7 @@ import com.ats.cataskapi.model.EmployeeListWithAvailableHours;
 public interface EmployeeListWithAvailableHoursRepo extends JpaRepository<EmployeeListWithAvailableHours, Integer> {
 
 	@Query(value = "select l.leave_id,l.cal_yr_id,l.emp_id,l.leave_duration,l.leave_fromdt,l.leave_todt,l.leave_num_days,"
-			+ "e.emp_name from  t_leave_apply l,m_emp e where e.emp_id=l.emp_id and ((l.leave_fromdt>=:fromDate and "
+			+ "e.emp_name from  t_leave_apply l,m_emp e where e.emp_id=l.emp_id and e.del_status=1 and l.del_status=1 and ((l.leave_fromdt>=:fromDate and "
 			+ "l.leave_fromdt<=:toDate) or (l.leave_todt>=:fromDate and l.leave_todt<=:toDate) or "
 			+ "(l.leave_fromdt<=:fromDate and l.leave_todt>=:toDate) or (l.leave_fromdt>=:fromDate "
 			+ "and l.leave_todt<=:toDate))", nativeQuery = true)
