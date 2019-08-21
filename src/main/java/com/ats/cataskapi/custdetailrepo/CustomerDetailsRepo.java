1fec92c5b14f7a720ba6ac17a32e42e435fb9ef2
maddy <maddy@maddy-Inspiron-3541>
2019-08-21 18:10:06 +0530

    //maddy

--- src/main/java/com/ats/cataskapi/custdetailrepo/CustomerDetailsRepo.java
+++ src/main/java/com/ats/cataskapi/custdetailrepo/CustomerDetailsRepo.java
@@ -11,19 +11,43 @@ import com.ats.cataskapi.model.CustomerDetails;
 @Repository
 public interface CustomerDetailsRepo extends JpaRepository<CustomerDetails, Integer> {
 	
-	@Query(value="SELECT m_cust_header.cust_id, m_cust_header.cust_firm_name,\n" + 
-			"		m_cust_header.cust_assessee_name,\n" + 
+	@Query(value="SELECT 0 as cust_group_name,\n" + 
+			" CASE WHEN m_cust_header.cust_group_id=0 THEN m_cust_header.cust_firm_name \n" + 
+			"	 \n" + 
+			"	  ELSE COALESCE(( SELECT m_cust_group.cust_group_name FROM m_cust_group WHERE m_cust_group.cust_group_id=m_cust_header.cust_group_id \n" + 
+			"	 \n" + 
+			"	  AND m_cust_group.del_status=1 ),0) END AS cust_firm_name,\n" + 
+			"        m_cust_header.cust_id,\n" + 
+			"        m_cust_header.cust_assessee_name,\n" + 
 			"        m_cust_header.cust_pan_no,\n" + 
 			"        m_cust_header.cust_email_id,\n" + 
 			"        m_cust_header.cust_phone_no,\n" + 
-			"        m_emp.emp_name,\n" + 
-			"		m_cust_group.cust_group_name\n" + 
+			"        m_emp.emp_name\n" + 
+			"    FROM\n" + 
+			"        m_cust_header,\n" + 
+			"        m_emp\n" + 
+			"    WHERE\n" + 
+			"        m_cust_header.del_status=1 \n" + 
+			"        AND    m_cust_header.owner_emp_id=m_emp.emp_id \n" + 
 			"        \n" + 
-			"FROM m_cust_header, m_emp, m_cust_group \n" + 
-			"WHERE m_cust_header.del_status=1 AND\n" + 
-			"	  m_cust_header.owner_emp_id=m_emp.emp_id AND\n" + 
-			"      m_cust_header.cust_group_id=m_cust_group.cust_group_id ORDER BY m_cust_header.cust_id DESC",nativeQuery=true)
+			"    ORDER BY\n" + 
+			"        m_cust_header.cust_id DESC",nativeQuery=true)
 	List<CustomerDetails> getAllCustomerDetails();
+	
+	/*SELECT m_cust_header.cust_id, m_cust_header.cust_firm_name,\n" + 
+	"		m_cust_header.cust_assessee_name,\n" + 
+	"        m_cust_header.cust_pan_no,\n" + 
+	"        m_cust_header.cust_email_id,\n" + 
+	"        m_cust_header.cust_phone_no,\n" + 
+	"        m_emp.emp_name,\n" + 
+	"		m_cust_group.cust_group_name\n" + 
+	"        \n" + 
+	"FROM m_cust_header, m_emp, m_cust_group \n" + 
+	"WHERE m_cust_header.del_status=1 AND\n" + 
+	"	  m_cust_header.owner_emp_id=m_emp.emp_id AND\n" + 
+	"      m_cust_header.cust_group_id=m_cust_group.cust_group_id ORDER BY m_cust_header.cust_id DESC*/
+	
+	/***************************************************************************/
 
 	@Query(value="SELECT\n" + 
 			"        m_cust_header.cust_id,\n" + 
--- src/main/java/com/ats/cataskapi/repositories/TaskListHomeRepo.java
+++ src/main/java/com/ats/cataskapi/repositories/TaskListHomeRepo.java
@@ -10,7 +10,8 @@ import com.ats.cataskapi.model.TaskListHome;
 
 public interface TaskListHomeRepo extends JpaRepository<TaskListHome, Integer> {
 
-	@Query(value="SELECT\n" + 
+	@Query(value="SELECT"
+			+ "\n" + 
 			"        t_tasks.task_id,\n" + 
 			"        t_tasks.task_text,\n" + 
 			"        t_tasks.task_start_date,\n" + 
@@ -24,26 +25,29 @@ public interface TaskListHomeRepo extends JpaRepository<TaskListHome, Integer> {
 			"        m_services.serv_name,\n" + 
 			"        m_activities.acti_name,\n" + 
 			"        dm_periodicity.periodicity_name,\n" + 
-			"        m_cust_group.cust_group_name,\n" + 
-			"        dm_fin_year.fin_year_name          \n" + 
+			"        CASE WHEN m_cust_header.cust_group_id=0 \n" + 
+			"        	THEN m_cust_header.cust_firm_name 	 \n" + 
+			"			  ELSE COALESCE(( SELECT m_cust_group.cust_group_name \n" + 
+			"                             FROM m_cust_group\n" + 
+			"                             WHERE m_cust_group.cust_group_id=m_cust_header.cust_group_id	 \n" + 
+			"	  						AND m_cust_group.del_status=1 ),0) END AS cust_group_name,\n" + 
+			"        dm_fin_year.fin_year_name               \n" + 
 			"    FROM\n" + 
 			"        t_tasks,\n" + 
 			"        m_emp,\n" + 
 			"        m_services,\n" + 
 			"        m_activities,\n" + 
 			"        dm_periodicity,\n" + 
-			"        m_cust_group,\n" + 
 			"        m_cust_header,\n" + 
-			"        dm_fin_year         \n" + 
+			"        dm_fin_year              \n" + 
 			"    WHERE\n" + 
-			"        t_tasks.del_status=1 \n" + 
-			"        AND         m_emp.emp_id=:empId \n" + 
-			"        AND   		FIND_IN_SET(:empId,t_tasks.task_emp_ids) \n" + 
-			"        AND         t_tasks.actv_id=m_activities.acti_id \n" + 
-			"        AND         t_tasks.serv_id=m_services.serv_id \n" + 
-			"        AND         m_activities.periodicity_id=dm_periodicity.periodicity_id \n" + 
-			"        AND         t_tasks.cust_id=m_cust_header.cust_id\n" + 
-			"        AND         m_cust_header.cust_group_id=m_cust_group.cust_group_id \n" + 
+			"        t_tasks.del_status=1          \n" + 
+			"        AND         m_emp.emp_id=:empId          \n" + 
+			"        AND     	FIND_IN_SET(:empId,t_tasks.task_emp_ids)          \n" + 
+			"        AND         t_tasks.actv_id=m_activities.acti_id          \n" + 
+			"        AND         t_tasks.serv_id=m_services.serv_id          \n" + 
+			"        AND         m_activities.periodicity_id=dm_periodicity.periodicity_id          \n" + 
+			"        AND         t_tasks.cust_id=m_cust_header.cust_id      \n" + 
 			"        AND         dm_fin_year.fin_year_id=t_tasks.task_fy_id", nativeQuery=true)
 	List<TaskListHome> getTaskList(@Param("empId") int empId);
 
@@ -151,8 +155,15 @@ public interface TaskListHomeRepo extends JpaRepository<TaskListHome, Integer> {
 			"        m_services.serv_name,\n" + 
 			"        m_activities.acti_name,\n" + 
 			"        dm_periodicity.periodicity_name,\n" + 
-			"        m_cust_group.cust_group_name,\n" + 
-			"        dm_fin_year.fin_year_name          \n" + 
+			"         CASE WHEN m_cust_header.cust_group_id=0\n" + 
+			"         	THEN m_cust_header.cust_firm_name \n" + 
+			"			ELSE COALESCE(( SELECT m_cust_group.cust_group_name\n" + 
+			"                        FROM m_cust_group \n" + 
+			"                                 WHERE m_cust_group.cust_group_id=m_cust_header.cust_group_id \n" + 
+			"	 \n" + 
+			"	  AND m_cust_group.del_status=1 ),0) \n" + 
+			"      END AS cust_group_name,\n" + 
+			"       dm_fin_year.fin_year_name               \n" + 
 			"    FROM\n" + 
 			"        t_tasks,\n" + 
 			"        m_emp,\n" + 
@@ -161,21 +172,61 @@ public interface TaskListHomeRepo extends JpaRepository<TaskListHome, Integer> {
 			"        dm_periodicity,\n" + 
 			"        m_cust_group,\n" + 
 			"        m_cust_header,\n" + 
-			"        dm_fin_year         \n" + 
+			"        dm_fin_year              \n" + 
 			"    WHERE\n" + 
-			"        t_tasks.del_status=1 \n" + 
-			"        AND         m_emp.emp_id=:empId\n" + 
-			"        AND  	  	 FIND_IN_SET(:empId,t_tasks.task_emp_ids) \n" + 
-			"        AND         t_tasks.task_start_date BETWEEN :fromDate AND :toDate  \n" + 
-			"        AND         m_services.serv_id=:service\n" + 
-			"        AND         m_activities.acti_id=:activity\n" + 
-			"        AND         m_cust_group.cust_group_id=:custId\n" + 
-			"        AND         t_tasks.actv_id=m_activities.acti_id \n" + 
-			"        AND         t_tasks.serv_id=m_services.serv_id \n" + 
-			"        AND         m_activities.periodicity_id=dm_periodicity.periodicity_id \n" + 
-			"        AND         t_tasks.cust_id=m_cust_header.cust_id\n" + 
-			"        AND         m_cust_header.cust_group_id=m_cust_group.cust_group_id \n" + 
+			"        t_tasks.del_status=1          \n" + 
+			"        AND         m_emp.emp_id=:empId         \n" + 
+			"        AND       	 FIND_IN_SET(:empId,t_tasks.task_emp_ids)          \n" + 
+			"        AND         t_tasks.task_start_date BETWEEN :fromDate AND :toDate   \n" + 
+			"        AND         m_services.serv_id=:service        \n" + 
+			"        AND         m_activities.acti_id=:activity       \n" + 
+			"        AND         m_cust_group.cust_group_id=:custId        \n" + 
+			"        AND         t_tasks.actv_id=m_activities.acti_id          \n" + 
+			"        AND         t_tasks.serv_id=m_services.serv_id          \n" + 
+			"        AND         m_activities.periodicity_id=dm_periodicity.periodicity_id          \n" + 
+			"        AND         t_tasks.cust_id=m_cust_header.cust_id         \n" + 
+			"        AND         m_cust_header.cust_group_id=m_cust_group.cust_group_id          \n" + 
 			"        AND         dm_fin_year.fin_year_id=t_tasks.task_fy_id", nativeQuery=true)
 	List<TaskListHome> getTaskList(@Param("empId") int empId, @Param("fromDate") String fromDate, @Param("toDate") String toDate,
 			@Param("service") int service, @Param("activity") int activity, @Param("custId") int custId);
+	
+	/*SELECT\n" + 
+	"        t_tasks.task_id,\n" + 
+	"        t_tasks.task_text,\n" + 
+	"        t_tasks.task_start_date,\n" + 
+	"        t_tasks.task_end_date,\n" + 
+	"        t_tasks.task_statutory_due_date,\n" + 
+	"        t_tasks.mngr_bud_hr,\n" + 
+	"        t_tasks.emp_bud_hr,\n" + 
+	"        t_tasks.task_emp_ids,\n" + 
+	"        t_tasks.task_status,\n" + 
+	"        m_emp.emp_name,\n" + 
+	"        m_services.serv_name,\n" + 
+	"        m_activities.acti_name,\n" + 
+	"        dm_periodicity.periodicity_name,\n" + 
+	"        m_cust_group.cust_group_name,\n" + 
+	"        dm_fin_year.fin_year_name          \n" + 
+	"    FROM\n" + 
+	"        t_tasks,\n" + 
+	"        m_emp,\n" + 
+	"        m_services,\n" + 
+	"        m_activities,\n" + 
+	"        dm_periodicity,\n" + 
+	"        m_cust_group,\n" + 
+	"        m_cust_header,\n" + 
+	"        dm_fin_year         \n" + 
+	"    WHERE\n" + 
+	"        t_tasks.del_status=1 \n" + 
+	"        AND         m_emp.emp_id=:empId\n" + 
+	"        AND  	  	 FIND_IN_SET(:empId,t_tasks.task_emp_ids) \n" + 
+	"        AND         t_tasks.task_start_date BETWEEN :fromDate AND :toDate  \n" + 
+	"        AND         m_services.serv_id=:service\n" + 
+	"        AND         m_activities.acti_id=:activity\n" + 
+	"        AND         m_cust_group.cust_group_id=:custId\n" + 
+	"        AND         t_tasks.actv_id=m_activities.acti_id \n" + 
+	"        AND         t_tasks.serv_id=m_services.serv_id \n" + 
+	"        AND         m_activities.periodicity_id=dm_periodicity.periodicity_id \n" + 
+	"        AND         t_tasks.cust_id=m_cust_header.cust_id\n" + 
+	"        AND         m_cust_header.cust_group_id=m_cust_group.cust_group_id \n" + 
+	"        AND         dm_fin_year.fin_year_id=t_tasks.task_fy_id*/
 }
