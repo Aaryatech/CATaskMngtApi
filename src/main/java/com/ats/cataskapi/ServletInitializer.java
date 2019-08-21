adefa239a6a59fe3429387731b2404a5f9ca74f0
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-08-21 15:18:23 +0530

    /Akshay

---
+++ src/main/java/com/ats/cataskapi/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.ats.cataskapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagementWebApiApplication.class);
+	}
+
+}
+
