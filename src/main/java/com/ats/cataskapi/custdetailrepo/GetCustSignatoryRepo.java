c57c6b88fb032ad830b0df4c06cb337f90f63676
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2019-08-21 18:10:00 +0530

    sign..sac

--- src/main/java/com/ats/cataskapi/custdetailrepo/GetCustSignatoryRepo.java
+++ src/main/java/com/ats/cataskapi/custdetailrepo/GetCustSignatoryRepo.java
@@ -10,7 +10,10 @@ import com.ats.cataskapi.model.custdetail.GetCustSignatory;
 
 public interface GetCustSignatoryRepo extends JpaRepository<GetCustSignatory, Integer> {
 
-	@Query(value=" SELECT m_cust_header.cust_firm_name,m_cust_signatory.signf_name,m_cust_signatory.signl_name," + 
+	@Query(value=" SELECT CASE WHEN m_cust_header.cust_group_id=0 THEN m_cust_header.cust_firm_name  " + 
+			"	    ELSE COALESCE(( SELECT m_cust_group.cust_group_name FROM m_cust_group WHERE m_cust_group.cust_group_id=m_cust_header.cust_group_id  " + 
+			"	   AND m_cust_header.cust_id=m_cust_signatory.cust_id AND m_cust_group.del_status=1 ),0) END AS cust_firm_name, " + 
+			"	   ,m_cust_signatory.signf_name,m_cust_signatory.signl_name," + 
 			" m_cust_signatory.sign_reg_no,m_cust_signatory.sign_design,m_cust_signatory.sign_phno," + 
 			" m_cust_signatory.contact_name,m_cust_signatory.contact_email,m_cust_signatory.contact_phno," + 
 			" m_cust_signatory.cust_id,m_cust_signatory.cust_remark,m_cust_signatory.sign_id " + 
@@ -21,7 +24,10 @@ public interface GetCustSignatoryRepo extends JpaRepository<GetCustSignatory, In
 			+ " order by m_cust_signatory.sign_id desc ", nativeQuery=true)
 	List<GetCustSignatory> getGetCustSignatoryByCustId(@Param("custId") int custId);
 
-	@Query(value=" SELECT m_cust_header.cust_firm_name,m_cust_signatory.signf_name,m_cust_signatory.signl_name," + 
+	@Query(value=" SELECT CASE WHEN m_cust_header.cust_group_id=0 THEN m_cust_header.cust_firm_name  " + 
+			"	    ELSE COALESCE(( SELECT m_cust_group.cust_group_name FROM m_cust_group WHERE m_cust_group.cust_group_id=m_cust_header.cust_group_id  " + 
+			"	   AND m_cust_header.cust_id=m_cust_signatory.cust_id AND m_cust_group.del_status=1 ),0) END AS cust_firm_name,"
+			+ " m_cust_signatory.signf_name,m_cust_signatory.signl_name," + 
 			" m_cust_signatory.sign_reg_no,m_cust_signatory.sign_design,m_cust_signatory.sign_phno," + 
 			" m_cust_signatory.contact_name,m_cust_signatory.contact_email,m_cust_signatory.contact_phno," + 
 			" m_cust_signatory.cust_id,m_cust_signatory.cust_remark,m_cust_signatory.sign_id " + 
