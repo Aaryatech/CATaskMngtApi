a57269a82dc7106f1e8afc22e10edd9566aa7f70
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-09-10 10:37:28 +0530

    /Akshay

--- src/main/java/com/ats/cataskapi/repositories/TaskCountByStatusRepo.java
+++ src/main/java/com/ats/cataskapi/repositories/TaskCountByStatusRepo.java
@@ -10,13 +10,52 @@ import com.ats.cataskapi.model.TaskCountByStatus;
 
 public interface TaskCountByStatusRepo extends JpaRepository<TaskCountByStatus, Integer>{
 
-	@Query(value="select s.status_mst_id,s.status_text,s.status_value, coalesce((select count(*) from t_tasks where task_end_date<:date "
-			+ "and s.status_value=task_status and FIND_IN_SET(:empId,task_emp_ids)),0) as overdeu, coalesce((select count(*) from t_tasks where "
-			+ "task_end_date=:date and s.status_value=task_status and  FIND_IN_SET(:empId,task_emp_ids)),0) as duetoday, coalesce ((select count(*) "
-			+ "from t_tasks where WEEKOFYEAR(task_end_date)=WEEKOFYEAR(:date)  and YEAR(:date) = "
-			+ "YEAR(task_end_date) and s.status_value=task_status and  FIND_IN_SET(:empId,task_emp_ids) ),0) as week  , coalesce ((select count(*) from t_tasks "
-			+ "where MONTH(task_end_date)=MONTH(:date) and YEAR(:date) = YEAR(task_end_date) and s.status_value=task_status and  "
-			+ "FIND_IN_SET(:empId,task_emp_ids)),0) as month from dm_status_mst s where del_status=1",nativeQuery=true)
+	@Query(value="select\n" + 
+			"        s.status_mst_id,\n" + 
+			"        s.status_text,\n" + 
+			"        s.status_value,\n" + 
+			"        coalesce((select\n" + 
+			"            count(*) \n" + 
+			"        from\n" + 
+			"            t_tasks \n" + 
+			"        where\n" + 
+			"            task_end_date<:date \n" + 
+			"            and s.status_value=task_status \n" + 
+			"            and FIND_IN_SET(:empId,task_emp_ids) and del_status = 1 and is_active=1),\n" + 
+			"        0) as overdeu,\n" + 
+			"        coalesce((select\n" + 
+			"            count(*) \n" + 
+			"        from\n" + 
+			"            t_tasks \n" + 
+			"        where\n" + 
+			"            task_end_date=:date \n" + 
+			"            and s.status_value=task_status \n" + 
+			"            and  FIND_IN_SET(:empId,task_emp_ids) and del_status = 1 and is_active=1),\n" + 
+			"        0) as duetoday,\n" + 
+			"        coalesce ((select\n" + 
+			"            count(*) \n" + 
+			"        from\n" + 
+			"            t_tasks \n" + 
+			"        where\n" + 
+			"            WEEKOFYEAR(task_end_date)=WEEKOFYEAR(:date)  \n" + 
+			"            and YEAR(:date) = YEAR(task_end_date) \n" + 
+			"            and s.status_value=task_status \n" + 
+			"            and  FIND_IN_SET(:empId,task_emp_ids) and del_status = 1 and is_active=1),\n" + 
+			"        0) as week  ,\n" + 
+			"        coalesce ((select\n" + 
+			"            count(*) \n" + 
+			"        from\n" + 
+			"            t_tasks \n" + 
+			"        where\n" + 
+			"            MONTH(task_end_date)=MONTH(:date) \n" + 
+			"            and YEAR(:date) = YEAR(task_end_date) \n" + 
+			"            and s.status_value=task_status \n" + 
+			"            and  FIND_IN_SET(:empId,task_emp_ids) and del_status = 1 and is_active=1),\n" + 
+			"        0) as month  \n" + 
+			"    from\n" + 
+			"        dm_status_mst s \n" + 
+			"    where\n" + 
+			"        del_status=1",nativeQuery=true)
 	List<TaskCountByStatus> getTaskCountByStatus(@Param("date") String date, @Param("empId") int empId);
 
 }
