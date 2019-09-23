03ea16fa5d36bb6fcf4c5c7e4b14a76730812d32
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-09-23 17:19:06 +0530

    /Akshay

--- src/main/java/com/ats/cataskapi/repositories/BugetedAmtAndRevenueRepo.java
+++ src/main/java/com/ats/cataskapi/repositories/BugetedAmtAndRevenueRepo.java
@@ -86,7 +86,7 @@ public interface BugetedAmtAndRevenueRepo extends JpaRepository<BugetedAmtAndRev
 			"    where\n" + 
 			"        e.emp_id =:empId",nativeQuery=true)*/
 	
-	@Query(value="select\n" + 
+	/*@Query(value="select\n" + 
 			"        case              \n" + 
 			"            when e.emp_type=3             then             coalesce((select\n" + 
 			"                sum(mngr_bud_hr)                       \n" + 
@@ -146,6 +146,72 @@ public interface BugetedAmtAndRevenueRepo extends JpaRepository<BugetedAmtAndRev
 			"        0) as actul_rev          \n" + 
 			"    from\n" + 
 			"        m_emp e      \n" + 
+			"    where\n" + 
+			"        e.emp_id =:empId",nativeQuery=true)*/
+	
+	@Query(value="select\n" + 
+			"        case                           \n" + 
+			"            when e.emp_type=3             then             coalesce((select\n" + 
+			"                sum(mngr_bud_hr)                                    \n" + 
+			"            from\n" + 
+			"                t_tasks                                    \n" + 
+			"            where\n" + 
+			"                task_end_date between :fromDate and :toDate                                                \n" + 
+			"                and FIND_IN_SET(e.emp_id,task_emp_ids)                                                \n" + 
+			"                and is_active=1                                                \n" + 
+			"                and del_status=1 \n" + 
+			"                and  cust_id in (:clntIds)),\n" + 
+			"            0)                                       \n" + 
+			"            when e.emp_type=5       then      coalesce((select\n" + 
+			"                sum(emp_bud_hr)                                   \n" + 
+			"            from\n" + 
+			"                t_tasks                                    \n" + 
+			"            where\n" + 
+			"                task_end_date between :fromDate and :toDate                                             \n" + 
+			"                and FIND_IN_SET(e.emp_id,task_emp_ids)                                                \n" + 
+			"                and is_active=1                                                \n" + 
+			"                and del_status=1 \n" + 
+			"                and cust_id in (:clntIds)),\n" + 
+			"            0)                          \n" + 
+			"            else             0                  \n" + 
+			"        end as bugeted_hrs ,\n" + 
+			"        coalesce((select\n" + 
+			"            sum(wl.work_hours)                            \n" + 
+			"        from\n" + 
+			"            t_daily_work_log wl,\n" + 
+			"            t_tasks t                           \n" + 
+			"        where\n" + 
+			"            wl.work_date between :fromDate and :toDate                                       \n" + 
+			"            and wl.emp_id=e.emp_id                                       \n" + 
+			"            and wl.del_status=1 \n" + 
+			"            and t.task_id=wl.task_id \n" + 
+			"            and t.cust_id in (:clntIds)),\n" + 
+			"        0) as actual_hrs,\n" + 
+			"        coalesce((select\n" + 
+			"           sum(t.billing_amt)                         \n" + 
+			"        from\n" + 
+			"            \n" + 
+			"            t_tasks t                            \n" + 
+			"        where\n" + 
+			"            t.del_status=1                                        \n" + 
+			"            and FIND_IN_SET(e.emp_id,t.task_emp_ids)                                        \n" + 
+			"            and t.task_end_date between :fromDate and :toDate                                        \n" + 
+			"            and is_active=1 and t.cust_id in (:clntIds)),\n" + 
+			"        0) as bugeted_rev,\n" + 
+			"        coalesce((select\n" + 
+			"            sum(t.billing_amt)                            \n" + 
+			"        from\n" + 
+			"            \n" + 
+			"            t_tasks t                            \n" + 
+			"        where\n" + 
+			"            t.del_status=1                                        \n" + 
+			"            and FIND_IN_SET(e.emp_id,t.task_emp_ids)                                        \n" + 
+			"            and t.task_end_date between :fromDate and :toDate                                   \n" + 
+			"            and is_active=1                     \n" + 
+			"            and t.task_status=9 and t.cust_id in (:clntIds)),\n" + 
+			"        0) as actul_rev               \n" + 
+			"    from\n" + 
+			"        m_emp e           \n" + 
 			"    where\n" + 
 			"        e.emp_id =:empId",nativeQuery=true)
 	BugetedAmtAndRevenue calculateBugetedAmtAndBugetedRevenue(@Param("empId") int empId,@Param("fromDate") String fromDate, 
